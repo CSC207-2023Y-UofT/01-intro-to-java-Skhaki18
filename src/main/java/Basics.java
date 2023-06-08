95e32a59569c5c01ecf28eb5af8f19c7210735cb
Skhaki18 <samir.khaki@mail.utoronto.ca>
2023-06-08 03:16:48 -0700

    Completed the tasks in Basics.java, all unit-tests pass

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,7 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
-
+        System.out.println("Hello World!");
 
 
         /* 4. In Python, we could declare variables using a simple assignment
@@ -62,7 +62,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -99,6 +99,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 10; i>=0; i--) {
+            System.out.println("Current count: " + i);
+        }
 
 
     }
@@ -141,6 +144,9 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
+        for (String str : to_split.split(" ")){
+            ret.append(str.charAt(0));
+        }
 
         // Fill in the rest of the body here
 
@@ -170,7 +176,10 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
-
+        if (arr.length < 1){ return 0;}
+        for (int i = 0; i < arr.length; i ++){
+            current_sum += (i%2==0) ? 0:arr[i];
+        }
         return current_sum;
     }
 
